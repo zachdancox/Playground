56e9e450ebd37a90f7de605b7dd41b471bfd5a48
Zach Cox <zach@Zachs-MacBook-Pro.local>
2017-11-21 22:49:17 -0600

    Played around with the Beautiful Soup library and followed a freecodecamp tutorial. Scrapped some stock data and exported it to a csv.

--- requesting.py
+++ requesting.py
@@ -1,5 +1,31 @@
 import requests
+# import libraries
+import urllib2
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
 
-r = requests.get('https://api.github.com/events')
+# specify the url
 
-print(r)
+quote_page = 'https://www.bloomberg.com/quote/INDU:IND'
+
+# query the website and return the html to the variable 'page'
+page = urllib2.urlopen(quote_page)
+
+# parse the html using beautiful soap and store in variable `soup`
+soup = BeautifulSoup(page, 'html.parser')
+
+# Find the ticker name and print it
+ticker = soup.find('div', attrs={'class': 'ticker'})
+ticker = ticker.text.strip()
+print ticker
+
+# Find the price of the ticker
+price = soup.find('div', attrs={'class': 'price'})
+print price.text
+
+# open a csv file with append, so old data will not be erased
+with open('index.csv', 'a') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow([ticker, price.text, datetime.now()])
+
